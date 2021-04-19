4c395a9e80b902dec5b338c652d39b34abb9b00f
Minh NQ <minh.tonn@gmail.com>
2021-04-19 10:36:45 +0700

    cm code

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,10 +2,4 @@ export = {
   host: process.env.DATABASE_CONNECTION,
   type: 'mongodb',
   database: process.env.DATABASE_DB_NAME,
-  migrations: [
-    'src/database/migrations/*.ts',
-  ],
-  cli: {
-    migrationsDir: 'src/database/migrations',
-  },
 };
